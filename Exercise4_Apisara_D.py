52463c0c92a868852c8230b84d287c3d98b96bed
apith9 <duangtimapisara@gmail.com>
2021-10-26 21:00:39 +0700

    Exercise4

---
+++ Exercise4_Apisara_D.py
@@ -0,0 +1,9 @@
+FE=80
+GB=90
+IS=95
+CP=100
+print("--- Your score ---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems : ",IS)
+print("Computer Programming : ",CP)
