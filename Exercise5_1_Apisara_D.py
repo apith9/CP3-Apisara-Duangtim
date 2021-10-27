93622691a3d840062d0b4b8ebf4b53989dbf2392
apith9 <duangtimapisara@gmail.com>
2021-10-27 16:09:28 +0700

    Exercise5

---
+++ Exercise5_1_Apisara_D.py
@@ -0,0 +1,10 @@
+fisrtNumber = int(input("1stNumber : "))
+secondNumber = int(input("2ndNumber : "))
+plus = fisrtNumber + secondNumber
+minus = fisrtNumber - secondNumber
+multiply = fisrtNumber * secondNumber
+divide = fisrtNumber / secondNumber
+print(fisrtNumber,"+",secondNumber,"=",plus)
+print(fisrtNumber,"-",secondNumber,"=",minus)
+print(fisrtNumber,"*",secondNumber,"=",multiply)
+print(fisrtNumber,"/",secondNumber,"=",divide)
---
+++ Exercise5_2_Apisara_D.py
@@ -0,0 +1,4 @@
+s = float(input("ระยะทาง(m): "))
+t = float(input("เวลา(s): "))
+v = s/t
+print(v,"m/s")
