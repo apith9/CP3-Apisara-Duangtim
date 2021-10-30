4e95d7abdb7bfbd47afbf14b512da900f4825ff0
apith9 <duangtimapisara@gmail.com>
2021-10-30 23:12:44 +0700

    Create Lecture50_Apisara_D.py

---
+++ Lecture50_Apisara_D.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def minus(x,y):
+    print(x-y)
+def multiply(x,y):
+    print(x*y)
+def divide(x,y):
+    print(x/y)
+addNumber(20,10)
+minus(20,10)
+multiply(20,10)
+divide(20,10)
